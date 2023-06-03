72b2a8f9ad1f25f464f9c0cd1c780e4346c31dc3
taagaar8 <Mirsije12345_>
2023-06-03 20:16:51 +0200

    first commit

---
+++ backend/src/test/java/ch/zhaw/iwi/devops/demo/PersonController.java
@@ -0,0 +1,12 @@
+package ch.zhaw.iwi.devops.demo;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class PersonController {
+    @GetMapping("/test")
+    public String test() {
+    return "Hello World app is up and running!";
+    }
+}
--- frontend/src/app/gui-model/guimodel.ts
+++ frontend/src/app/gui-model/guimodel.ts
@@ -87,6 +87,28 @@ export class GuiModel {
                         }
                     ]
                 },
+                {
+                    "id": "ModuleForm",
+                    "title": "Type your Module Information",
+                    "formFieldList": [
+                    {
+                        "id":   "title",
+                        "type": "text",
+                        "name": { default: "Titel" },
+                        "required": true,
+                        "width": 2
+                    },
+                    {
+                        "id": "description",
+                        "type": "text",
+                        "name": { default: "Text" },
+                        "newRow": true,
+                        "maxLength": 4000,
+                        "height": 4,
+                        "width": 2
+                    }
+                    ]
+                    }
             ],
             "pageList": [
                 {
@@ -107,6 +129,14 @@ export class GuiModel {
                             "width": 2,
                             "newRow": true,
                         },
+                        {
+                            "type": "button",
+                            "name": { default: "Module" },
+                            "icon": "fa-file-alt",
+                            "color": "wet-asphalt",
+                            "page": "modulePage",
+                            "width": 2,
+                            },
                     ]
                 },
                 {
@@ -138,6 +168,27 @@ export class GuiModel {
                         }
                     ]
                 },
+                {
+                    "id": "modulePage",
+                    "elementList": [
+                    {
+                    "type": "backbutton",
+                    },
+                    {
+                    "type": "newButton",
+                    "name": { default: "Neues Modul" },
+                    "icon": "fa-user",
+                    "color": "green",
+                    "width": 2,
+                    "form" : {
+                    "form" : "ModuleForm"
+                    }
+                    },
+                    
+                    ]
+                    
+                    },
+                    
             ]
         }
     };
