671848c7a331888574404f8bccb785a1c66ce924
taagaar8 <Mirsije12345_>
2023-06-04 07:42:42 +0200

    Convert Number 5 with new HashMap

--- backend/src/main/java/ch/zhaw/iwi/devops/romannumerals/RomanNumeralsConverter.java
+++ backend/src/main/java/ch/zhaw/iwi/devops/romannumerals/RomanNumeralsConverter.java
@@ -1,22 +1,56 @@
 package ch.zhaw.iwi.devops.romannumerals;
+import java.util.LinkedHashMap;  
+import java.util.Map;  
 
 public class RomanNumeralsConverter {
-    
+
     public String convertToRoman(int value) {
-        if(value==1){
-            return "I";
-        }
-        if(value==2){
-            return "II";
-        }
-        if(value==3){
-            return "III";
-        }
-        if(value==4){
-            return "IV";
-        }
-        else{
-            return "value not known";
-        }
-        }
+        
+        LinkedHashMap<String, Integer> romanNumerals = new LinkedHashMap<String, Integer>();  
+        //storing roman letters and corresponding decimal values in HashMap  
+        romanNumerals.put("M", 1000);  
+        romanNumerals.put("CM", 900);  
+        romanNumerals.put("D", 500);  
+        romanNumerals.put("CD", 400);  
+        romanNumerals.put("C", 100);  
+        romanNumerals.put("XC", 90);  
+        romanNumerals.put("L", 50);  
+        romanNumerals.put("XL", 40);  
+        romanNumerals.put("X", 10);  
+        romanNumerals.put("IX", 9);  
+        romanNumerals.put("V", 5);  
+        romanNumerals.put("IV", 4);  
+        romanNumerals.put("I", 1);  
+
+        //variable for string the result  
+        String result = "";  
+
+        //loop iterate over Map  
+        for(Map.Entry<String, Integer> entry : romanNumerals.entrySet())  
+        {  
+        int matches = value/entry.getValue();  
+        result = result+repeat(entry.getKey(), matches);  
+        value = value % entry.getValue();  
+        }  
+        return result;  
+        }  
+
+
+        public static String repeat(String s, int n)   
+        {  
+            if(s == null)   
+        {  
+        return null;  
+        }  
+
+        final StringBuilder sb = new StringBuilder();  
+
+        for(int i = 0; i < n; i++)   
+        {  
+            sb.append(s);  
+        }  
+        //converts into string  
+        return sb.toString();  
+        }  
+
 }
